0622a7d098234b254938bfd4ec3cd0ccfc5ef67a
Egor Elkin <elkinegora@gmail.com>
2018-11-15 20:48:48 +0500

    третье задание, запускается из терминала командой node third_task.js

---
+++ lesson_first/third_task/third_task.js
@@ -0,0 +1,22 @@
+function fileConverter()
+{
+    const fs = require("fs");
+    let inputFile = "example.txt";
+    console.time("Время конвертации файла");
+    fs.readFile(inputFile, "utf8", ( err, data ) => {
+       if ( err ) {
+           console.log( err.message );
+       } else {
+           let trimmedData = data.replace(/\s+/g, ' ');
+           fs.writeFile("converted_" + inputFile, trimmedData, 'utf8', ( outputErr, outputData ) => {
+               if ( outputErr ) {
+                   console.log( outputErr );
+               } else {
+                   return outputData;
+               }
+           });
+       }
+    });
+    console.timeEnd("Время конвертации файла");
+}
+fileConverter();
